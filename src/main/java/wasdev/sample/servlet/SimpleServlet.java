cd4a46cd71f0130f565e19c9c73dd77d98eed27b
whaenel <vonhenninghausen@web.de>
2017-01-03 17:10:35 +0100

    Added Sum

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -22,6 +22,7 @@ public class SimpleServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.getWriter().print("Hello Walters GitWorld!");
+        response.getWriter().print("<br/> 3 + 4 Sum is  7");
    }
 
 }
